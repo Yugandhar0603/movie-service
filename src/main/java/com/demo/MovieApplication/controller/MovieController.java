9b1bcc64031d4058ef888d278affbf0099a713d8
Yugandhar Reddy Allu <106137008+yugandhar123456789@users.noreply.github.com>
2025-01-09 09:32:32 +0530

    commited changes

---
+++ src/main/java/com/demo/MovieApplication/controller/MovieController.java
@@ -0,0 +1,45 @@
+package com.demo.MovieApplication.controller;
+
+
+import com.demo.MovieApplication.pojo.MoviePojo;
+import com.demo.MovieApplication.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+
+    @GetMapping("/books")
+    public ResponseEntity<List<MoviePojo>> getAllMovies(){
+        return new ResponseEntity<List<MoviePojo>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/books/{mid}")
+    public ResponseEntity<MoviePojo> getAMovie(@PathVariable("mid") int movieId) {
+        return new ResponseEntity<MoviePojo>(movieService.getAMovie(movieId), HttpStatus.OK);
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<MoviePojo> addMovie(@RequestBody MoviePojo moviePojo) {
+        return new ResponseEntity<MoviePojo>(movieService.addMovie(moviePojo), HttpStatus.OK);
+    }
+
+    @PutMapping("/books")
+    public ResponseEntity<MoviePojo> updateMovie(@RequestBody MoviePojo moviePojo) {
+        return new ResponseEntity<MoviePojo>(movieService.updateMovie(moviePojo), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/books/{mid}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable("mid") int movieId) {
+        movieService.deleteMovie(movieId);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+}
